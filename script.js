35a4402a50fdb19b90a81680243643a95b30625d
x23004akiyama <x23004xx@aitech.ac.jp>
2024-07-16 22:32:42 +0900

    更新

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-//logoの表示
-$(window).on('load',function(){
-    $("#splash").delay(2000).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-    $("#splash_logo").delay(2000).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-  });
+// logoの表示
+$(window).on('load', function(){
+    if (!sessionStorage.getItem('splashDisplayed')) {
+        $("#splash").delay(2000).fadeOut('slow'); // ローディング画面を2秒（2000ms）待機してからフェードアウト
+        $("#splash_logo").delay(2000).fadeOut('slow'); // ロゴを2秒（2000ms）待機してからフェードアウト
+        sessionStorage.setItem('splashDisplayed', 'true');
+    } else {
+        $("#splash").hide(); // スプラッシュスクリーンを表示しない
+    }
+
+    // ヘッダーアニメーションの制御
+    if (!sessionStorage.getItem('headerAnimationDisplayed')) {
+        $('header').addClass('header-animation');
+        sessionStorage.setItem('headerAnimationDisplayed', 'true');
+    } else {
+        $('header').addClass('header-no-animation');
+    }
+});
+
+// メニューの "HOME" リンクをクリックしたときにセッションストレージを設定
+$('nav a[href="home.html"]').on('click', function() {
+    sessionStorage.setItem('headerAnimationDisplayed', 'true');
+});
